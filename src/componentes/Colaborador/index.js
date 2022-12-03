2beea7b47bea5648f6ade4501809fb6d5c0342ce
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2022-12-03 10:59:57 -0300

    feat: colaborador component

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img src='https://github.com/TiagoTegon.png' alt='Tiago Tegon' />
+      </div>
+      <div className='rodape'>
+        <h4>Tiago Tegon</h4>
+        <h5>Desenvolvedor Júnior</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Colaborador
